60d9bfbd2299818b6932a8b27233f7119c5d3186
adhamme80739035 <adhammedhat9@gmail.com>
2022-12-22 12:30:56 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+
+{
+
+	/* your code goes there */
+	char c = 'z';
+
+
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
