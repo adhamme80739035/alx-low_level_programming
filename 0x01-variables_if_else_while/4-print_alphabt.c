9816a99d5a6bfa8074ba52930d0f294c5b559184
adhamme80739035 <adhammedhat9@gmail.com>
2022-12-22 06:16:25 -0800

    exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+
+{
+
+	/* your code goes there */
+	char letter;
+
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if(letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
