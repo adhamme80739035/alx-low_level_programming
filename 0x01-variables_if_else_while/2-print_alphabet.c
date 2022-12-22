6fda53a9ba53156c75790dc0f2ff4ddaacdd59ba
adhamme80739035 <adhammedhat9@gmail.com>
2022-12-22 05:03:11 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+
+{
+
+	/* your code goes there */
+	char letter;
+
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+			putchar(letter);
+
+
+	putchar('\n');
+
+	
+
+	return (0);
+
+}
